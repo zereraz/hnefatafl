e2c984df29e5925244f740dd4439bfcc223f784a
Sathya <sahebjot94@gmail.com>
2015-08-09 00:37:05 +0530

    change env name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
